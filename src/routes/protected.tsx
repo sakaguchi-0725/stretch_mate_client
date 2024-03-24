3c58dba6d9339f0b62fd1232605dafe808edceb7
sakaguchi <kzmsan63@gmail.com>
2024-03-24 22:27:05 +0900

    home画面追加

--- src/routes/protected.tsx
+++ src/routes/protected.tsx
@@ -1,5 +1,6 @@
 import MainLayout from "@/components/Layout/MainLayout";
 import ExampleRoute from "@/features/example/routes";
+import { HomeRoutes } from "@/features/home";
 import { Outlet } from "react-router-dom";
 
 const App = () => {
@@ -14,6 +15,9 @@ export const protectedRoutes = [
   {
     path: "/app",
     element: <App />,
-    children: [{ path: "example", element: <ExampleRoute /> }],
+    children: [
+      { path: "home", element: <HomeRoutes /> },
+      { path: "example", element: <ExampleRoute /> }
+    ],
   },
 ];
